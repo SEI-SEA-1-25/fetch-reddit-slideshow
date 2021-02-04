4db5d6d05ce973219f7305ef499c03113ea90485
semuel1 <semuelst1@gmail.com>
2021-02-04 09:57:10 -0800

    Fetch

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// app state//
+const Timer_Speed = 1000
+
+//var to store set INtervel
+let slideShowInterval = null
+
+//img
+let photos = []
+
+//index of current
+let photoIndex = 0
+
+//dom selectors
+const SEARCH_FORM = document.querySelector(#'search-form')
+const SEARCH_INPUT = document.querySelector(#'search-input')
+const SUBMIT_BUTTON = document.querySelector(#'submit-button')
+const STOP_BUTTON = document.querySelector(#'stop-button')
+const SLIDESHOW_CONTAINER = document.querySelector('slideshow-container')
+//functions
